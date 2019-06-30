1b6c096041690278cd0d25c3f8f696ec32a53813
Usman Adio <usthmandanfodio@gmail.com>
2019-06-30 09:49:47 -0700

    ch (server): Set up the database
    -Create and set up a database
    -Create tables for the necessary database
    -Create  scripts for migrations to and from the database
    
    [Delivers #166997745 ]

---
+++ src/model/migrations/down.js
@@ -0,0 +1,29 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+const { Pool } = pg;
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('connect', () => {
+  console.log('connected to the database');
+});
+
+/**
+ * Drop Tables
+ */
+const dropTable = async (queryText) => {
+  await pool.query(queryText);
+  pool.end();
+};
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+const QueryText = 'DROP TABLE IF EXISTS users, buses, trips, bookings';
+dropTable(QueryText);
